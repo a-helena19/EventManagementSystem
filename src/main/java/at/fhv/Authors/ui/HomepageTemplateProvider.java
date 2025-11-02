5089deb0f67f61c8b1148fb9c145375b473af560
Hla Aldlol <e12106330@student.tuwien.ac.at>
2025-11-02 21:01:59 +0100

    create Event method added to homepage

--- src/main/java/at/fhv/Authors/ui/HomepageTemplateProvider.java
+++ src/main/java/at/fhv/Authors/ui/HomepageTemplateProvider.java
@@ -1,12 +1,24 @@
 package at.fhv.Authors.ui;
 
+import at.fhv.Authors.domain.model.Event;
+import at.fhv.Authors.persistence.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 
 @Controller
 public class HomepageTemplateProvider {
+
+    @Autowired
+    private EventRepository eventRepository;
+
     @GetMapping("/homepage")
     public ModelAndView getHomepageTemplate() {
         // Test if it gets here
@@ -15,4 +27,21 @@ public class HomepageTemplateProvider {
 
         return new ModelAndView("Homepage");
     }
+
+
+    @PostMapping("/create-event")
+    public String createEvent(
+            @RequestParam String name,
+            @RequestParam(required = false) String description,
+            @RequestParam String location,
+            @RequestParam LocalDate date,
+            @RequestParam BigDecimal price
+    ) {
+        System.out.println("Creating new Event: " + name);
+        Event newEvent = new Event(name, description, location, date, price);
+        eventRepository.save(newEvent);
+
+        return "redirect:/homepage";
+    }
+
 }
