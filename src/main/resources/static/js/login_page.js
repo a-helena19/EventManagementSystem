bd85f84880a8d662d95959fe5c4946cbf4a3435e
davfli <dfl5493@students.fhv.at>
2025-11-05 11:08:08 +0100

    Usertest.html , login_page und CSS weitergearbeitet: Sign up -> Sign In

--- src/main/resources/static/js/login_page.js
+++ src/main/resources/static/js/login_page.js
@@ -1,11 +1,28 @@
 const container = document.getElementById('container');
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpButton = document.getElementById('register');
+const signInButton = document.getElementById('login');
+
+
+
+
 
 signUpButton.addEventListener('click', () => {
+
     container.classList.add("active");
 });
 
 signInButton.addEventListener('click', () => {
+
     container.classList.remove("active");
 });
+
+// soll theoretisch helfen?: noch keine notwendigkeit gefunden
+// signUpButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     container.classList.add("active");
+// });
+//
+// signInButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     container.classList.remove("active");
+// });
